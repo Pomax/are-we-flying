a36f54ee1ed2f3abc95451a6ac2be65ebb6b6731
Pomax <pomax@nihongoresources.com>
2023-10-24 16:43:20 -0700

    lol

--- src/api/api-server.js
+++ src/api/api-server.js
@@ -10,15 +10,6 @@ import { linkClasses } from "socketless";
 const factory = linkClasses(ClientClass, ServerClass);
 const { webserver } = factory.createServer();
 
-webserver.addRoute(`/`, (_, res) => {
-  res.writeHead(200, { "Content-Type": `text/html` });
-  res.end(
-    `<!doctype html><p>API server status: running, MSFS${
-      api.connected ? `` : ` not`
-    } connected${api.connected ? `` : ` (yet)`}.</p>`
-  );
-});
-
 webserver.listen(API_PORT, () => {
   console.log(`Server listening on http://localhost:${API_PORT}`);
 });
