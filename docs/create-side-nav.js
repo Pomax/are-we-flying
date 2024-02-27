fe941a5fc0d5c34bd5e3bd084eba6d486bcced30
Pomax <pomax@nihongoresources.com>
2024-02-27 08:42:40 -0800

    nav menu

--- docs/create-side-nav.js
+++ docs/create-side-nav.js
@@ -3,3 +3,6 @@ const nav = document.getElementById(`nav-menu`);
 const copied = toc.cloneNode(true);
 copied.id = `nav-toc`;
 nav.appendChild(copied);
+
+document.querySelector(`h1.table-of-contents`).remove();
+toc.remove();
