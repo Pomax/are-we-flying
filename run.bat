1c130840081a245afae6335e7e1f04ae8caa6f80
Pomax <pomax@nihongoresources.com>
2024-08-04 08:50:40 -0700

    .

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
-start "API Server" cmd /k "node api-server.js %1"
-start "Web Server" cmd /k "node web-server.js --owner --browser"
+start "API Server" cmd /k "cd parts/part 6/auto-landing && node api-server.js %1"
+start "Web Server" cmd /k "cd parts/part 6/auto-landing && node web-server.js --owner --browser"
